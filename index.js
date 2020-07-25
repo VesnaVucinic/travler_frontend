66ba5c802b667f25be4e39710ba4a76a14cefd15
h-miiaah <haim.miiaah5@gmail.com>
2020-07-24 21:41:08 -0400

    adjusted getLocations finction to add the locatoins to a div on the page, and added div with id location container to index.html file

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ function getLocations() {
   fetch(baseURL)
     .then((response) => response.json())
     .then((locations) => {
-      console.log(locations);
+      locations.data.forEach((location) => {
+        const locationMarkup = `
+            <div data-id="${location.id}">
+                <h2>${location.attributes.name}</h2>
+                <h4>${location.attributes.destination.name}</h4>
+                <p>${location.attributes.description}</p>
+                <img src="${location.attributes.image_url}" height="400" width="500">
+                <br>
+                <button data-id="${location.id}">Edit</button>
+            </div>
+            <br>
+            <br>
+          `;
+        document.querySelector(
+          "#location-container"
+        ).innerHTML += locationMarkup;
+      });
     });
 }
