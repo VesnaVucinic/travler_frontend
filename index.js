f7e5e7e766d461ab0676e1b4e9ee2d1097095dbf
h-miiaah <haim.miiaah5@gmail.com>
2020-07-24 21:19:18 -0400

    added getLocations function

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const baseURL = "http://localhost:3000/api/v1/locations";
+
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("loaded");
+  getLocations();
 });
+
+function getLocations() {
+  fetch(baseURL)
+    .then((response) => response.json())
+    .then((locations) => {
+      console.log(locations);
+    });
+}
