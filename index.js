9cf918a1ef94a9dd5a415b955522dd81f3e5c865
h-miiaah <haim.miiaah5@gmail.com>
2020-07-25 22:29:03 -0400

    added POST fetch request with HTML markup in index js file

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function getLocations() {
+  // GET request
   fetch(baseURL)
     .then((response) => response.json())
     .then((locations) => {
@@ -38,6 +39,41 @@ function createFormHandler(e) {
   const locationDescriptionInput = document.querySelector("#input-description")
     .value;
   const locationImageInput = document.querySelector("#input-url").value;
-  const locationDestinationInput = document.querySelector("#destinations")
-    .value;
+  const locationDestinationInputId = parseInt(
+    document.querySelector("#destinations").value
+  );
+  postFetch(
+    locationNameInput,
+    locationDescriptionInput,
+    locationImageInput,
+    locationDestinationInputId
+  );
+}
+
+function postFetch(name, description, image_url, destination_id) {
+  const bodyData = { name, description, image_url, destination_id };
+  // POST request
+  fetch(baseURL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(bodyData),
+  })
+    .then((response) => response.json())
+    .then((location) => {
+      // console.log(location);
+      const locationData = location.data;
+      const locationMarkup = `
+            <div data-id="${locationData.id}">
+                <h2>${locationData.attributes.name}</h2>
+                <h4>${locationData.attributes.destination.name}</h4>
+                <p>${locationData.attributes.description}</p>
+                <img src="${locationData.attributes.image_url}" height="400" width="500">
+                <br>
+                <button data-id="${locationData.id}">Edit</button>
+            </div>
+            <br>
+            <br>
+          `;
+      document.querySelector("#location-container").innerHTML += locationMarkup;
+    });
 }
